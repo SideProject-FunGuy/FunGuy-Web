881d3950abcf79999e499cb2f6388faadc9d67bb
Sojung Choi <sojc31@gmail.com>
2020-12-26 19:51:59 -0800

    commented out section testing db connection

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -3,27 +3,27 @@ const readLine = require('readline');
 const MongoClient = require('mongodb').MongoClient;
 
 let uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wktan.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const dbName = 'funguy';
 
 async function run(){
   try {
     await client.connect();
-    console.log('connected correctly to server');
+    console.log('Connected correctly to server');
     const db = client.db(dbName);
 
-    const col = db.collection('users');
-
-    let personDocument = {
-      "email": "test2"
-    }
-
-    const p = await col.insertOne(personDocument);
-
-    const myDoc = await col.findOne();
-
-    console.log(myDoc);
+    // const col = db.collection('users');
+    //
+    // let personDocument = {
+    //   "email": "test4@hotmail.com"
+    // }
+    //
+    // const p = await col.insertOne(personDocument);
+    //
+    // const myDoc = await col.findOne();
+    //
+    // console.log(myDoc);
 
   } catch (err) {
     console.log(err.stack);
