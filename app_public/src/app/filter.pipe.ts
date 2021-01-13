fcf678498eaf167daccdc25aa1e0d8ba18773ef3
Sojung Choi <sojc312gmail.com>
2021-01-12 18:00:04 -0800

    created filter pipe

--- app_public/src/app/filter.pipe.ts
+++ app_public/src/app/filter.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(items: any[], searchText: string): any[] {
+    if(!items) return [];
+    if(!searchText) return items;
+    return items.filter( it => {
+      return it.category.includes(searchText);
+    });
   }
-
 }
